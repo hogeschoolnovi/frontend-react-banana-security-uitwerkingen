293f48b893a18ffb51d3b3ada51b4c586fce9e08
S-Barnhoorn <sambarnhoorn@gmail.com>
2023-11-02 12:58:44 +0100

    controlled components for signin and signup

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,28 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 function SignIn() {
-  const { login } = useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    login();
-  }
+    const { login } = useContext(AuthContext);
 
-  return (
-    <>
-      <h1>Inloggen</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+    function handleSubmit(e) {
+        e.preventDefault();
 
-      <form onSubmit={handleSubmit}>
-        <p>*invoervelden*</p>
-        <button type="submit">Inloggen</button>
-      </form>
+        console.log({
+            email: email,
+            wachtwoord: password,
+        });
 
-      <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
-    </>
-  );
+        login();
+    }
+
+    return (
+        <>
+            <h1>Inloggen</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email-field">
+                    Emailadres:
+                    <input
+                        type="email"
+                        id="email-field"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+
+                <label htmlFor="password-field">
+                    Wachtwoord:
+                    <input
+                        type="password"
+                        id="password-field"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+
+                <button
+                    type="submit"
+                    className="form-button"
+                >
+                    Inloggen
+                </button>
+            </form>
+
+            <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+        </>
+    );
 }
 
 export default SignIn;
